9e823da4711fe33893bf318b049d2d70ae40cae2
Saugat Joshi <jsaugatt02@gmail.com>
2024-11-08 22:37:06 +0545

    update auth and routes files

---
+++ routes.ts
@@ -0,0 +1,17 @@
+export const publicRoutes = [
+  // User can change their email from the settings page while being logged in so... this route must be public 
+  "/auth/verify-email",
+  "/auth/new-password"
+]
+export const authRoutes = ["/auth/login", "/auth/signup"]
+export const apiAuthPrefix = "/api/auth";
+export const DEFAULT_LOGIN_REDIRECT = "/"
+
+/**
+ * Route and configuration constants for authentication:
+ *
+ * - `publicRoutes`: Defines routes accessible without authentication (e.g., home page).
+ * - `authRoutes`: Contains routes specifically for authentication actions, such as login and signup.
+ * - `apiAuthPrefix`: Prefix path for API routes handling authentication-related requests (e.g., login and signup API endpoints).
+ * - `DEFAULT_LOGIN_REDIRECT`: Default route to redirect users after a successful login.
+ */
